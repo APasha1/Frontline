1b1a2d599333f0b6b9600c730966775a0507ff2d
Glitch (hello-express) <none>
2021-07-24 03:56:22 +0000

    🍲🌵 Checkpoint ./commands/genkey.js:679300/1205 ./server.js:679300/4

--- commands/genkey.js
+++ commands/genkey.js
@@ -1,12 +1,22 @@
 const discord = require("discord.js");
 
+function genID() {
+  var d = new Date().getTime();
+  var d2 = d
+  if (typeof d2 !== 'undefined' && typeof d2.now === 'function'){
+    d += performance.now();
+  }
+  return 'ODERA-xyxyx-yyxxx-yyxyy-xyyyx'.replace(/[xy]/g, function (c) {
+    var r = (d + Math.random() * 16) % 16 | 0;
+    d = Math.floor(d / 16);
+    return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+  });
+}
+
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-  const msg = await message.channel.send("Pinging...");
-  const embed = new discord.MessageEmbed()
-  embed.setColor(client.config.embedColors.default)
-  let latency = ((msg.createdTimestamp - message.createdTimestamp) / 1000).toFixed(2)
-  embed.setDescription(`**Response speed: ${latency} seconds**`)
-  msg.edit({embed})
+  let memberToBindTo = message.mentions.members.first()
+  if (!memberToBindTo) return message.channel.send("Please provide a user to generate a key for.")
+  message.channel.send("Alright, generated a key for " + memberToBindTo.user.discriminator + ".\nThis will allow them to use Autoranking for **one** group.")
 };
 
 exports.conf = {
