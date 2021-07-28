be647d9c999ba2de5c80547dfe1fa1452c076c34
Glitch (hello-express) <none>
2021-07-28 03:33:58 +0000

    🚤🌾 Checkpoint ./commands/whitelist.js:679300/1151 ./package.json:679300/1902 ./commands/genkey.js:679300/96

--- commands/genkey.js
+++ commands/genkey.js
@@ -23,8 +23,8 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
   }
   
   let productList = productsToList.join("\n")
-  if (!memberToBindTo) return message.channel.send("Please provide a user to generate a key for.")
-  if (!client.config.products[product]) return message.channel.send("Please provide a valid **product** to bind this key to. You can provide these:\n\n" + productList)
+  if (!memberToBindTo) return message.channel.send(`${client.config.emotes.deny} Please provide a user to generate a key for.`)
+  if (!client.config.products[product]) return message.channel.send(`${client.config.emotes.deny} Please provide a valid **product** to bind this key to. You can provide these:\n\n` + productList)
   
   let properName = client.config.products[product].name
   let newKey = genID()
--- commands/whitelist.js
+++ commands/whitelist.js
@@ -1,9 +1,32 @@
 const discord = require("discord.js");
+const roblox = require("noblox");
 
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   let productKey = args[0]
   let action = args[1]
-  let targetId = args[2]
+  let targetId = Number(args[2])
+  
+  client.getData(productKey).then(keyData => {
+    if (keyData.user == message.author.id) {
+      if (action == "add") {
+        roblox.getGroup({userId: targetId}).then(result => {
+          
+        }).catch(err => {
+          roblox.getPlayerInfo({userId: targetId}).then(result => {
+            message.channel.send(client.config.emotes.accept + "")
+          }).catch(err => {
+            message.channel.send(client.config.emotes.deny + " The ID provided was invalid. Please provide a valid **User ID or Group ID.**")
+          })
+        })
+      } else if (action == "remove") {
+        
+      }
+    } else {
+      message.channel.send(client.config.emotes.deny + " You do not have permission to edit the whitelisting for this key.")
+    }
+  }).catch(err => {
+    message.channel.send(client.config.emotes.deny + " This key does not exist in the database. Please make sure you've spelt it correctly.")
+  })
 };
 
 exports.conf = {
