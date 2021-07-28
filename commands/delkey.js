89a2b61dc7850f274ace0d019d758e9337a2e93b
Glitch (hello-express) <none>
2021-07-28 04:42:34 +0000

    💵⏱ Checkpoint ./commands/delkey.js:679300/1034

--- commands/delkey.js
+++ commands/delkey.js
@@ -2,14 +2,13 @@ const discord = require("discord.js");
 
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   let key = args[0]
-  try {
-    let keyExists = await client.getData(key)
-    client.redisClient.del(key).then(a => {
+  client.redisClient.del(key).then(result => {
+    if (result > 0) {
       message.channel.send(`${client.config.emotes.accept} The key was deleted.`)
-    })
-  } catch {
-    message.channel.send(`${client.config.emotes.accept} The key does not exist in the database.`)
-  }
+    } else {
+      message.channel.send(`${client.config.emotes.deny} The key does not exist in the database.`)
+    }
+  })
 };
 
 exports.conf = {
