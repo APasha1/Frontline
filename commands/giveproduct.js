853105ff8dc3a94bd372757efc781cbed9eefdf6
Glitch (hello-express) <none>
2021-10-17 23:02:10 +0000

    👄🏜 Checkpoint ./commands/giveproduct.js:29953541/236 ./commands/help.js:29953541/190

---
+++ commands/giveproduct.js
@@ -0,0 +1,14 @@
+const discord = require('discord.js')
+
+exports.run = async(client, message, args, level) => {
+  
+  
+  
+}
+
+exports.conf = {
+  enable: true,
+  guildOnly: true,
+  aliases: ["gp"],
+  permlevel: "Staff`"
+}
