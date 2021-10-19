286d5f3dbeda0431b4ecbc0ec122e39b5fcea50c
Glitch (hello-express) <none>
2021-10-19 22:27:32 +0000

    🌧🏞 Checkpoint ./config.js:29953541/32

--- config.js
+++ config.js
@@ -63,7 +63,7 @@ var config = {
   },
   
   emotes: {
-    accept: "<a:accepted:868335596165271603>",
+    accept: "<a:accepted:868335596165271603>", //what are u doing noob shush
     deny: "<a:denied:868335596299497483>",
     maybe: "<a:maybe:868335596567928842>",
     loading: "<a:loading:868335596362399784>",
