f0504eca0c4c1512aeebbaa45282a63410d5773a
Glitch (ostudioss) <none>
2022-09-19 18:13:02 +0000

    🍠🕹 Checkpoint ./commands/embed.js:29953541/627

--- commands/embed.js
+++ commands/embed.js
@@ -3,12 +3,19 @@ const discord = require("discord.js");
 
 
 exports.run = async (client, message, args, level) => { 
-const channel = message.mentions.channels.first()
-if(channel){
-  
+    const MyMessage = message.content.slice(30).trim();
+    
+    let announceChannel = message.mentions.channels.first();
+    if(announceChannel){
+    const embed = new discord.MessageEmbed()
+    .setDescription(MyMessage)
+    .setColor("BLUE")
+    announceChannel.send({embed})
+      message.channel.send("Message sent!")
+    }else{
+      message.channel.send("Please enter the command with the right format. It would be: o!embed <#channel> <msg>")
+    }
 }
-}
-
 
 exports.conf = {
   enabled: true,
